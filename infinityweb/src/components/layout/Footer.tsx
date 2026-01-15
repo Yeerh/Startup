59e16e1ad3bfce2100e296589b15888408411c44
Diego Ewerton <diegogeniu@gmail.com>
2026-01-14 23:49:33 -0300

    atualizar

--- infinityweb/src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="py-10 text-center text-white/50 border-t border-white/10">
-      © {new Date().getFullYear()} InfinityWeb. Todos os direitos reservados.
-    </footer>
-  );
-}
